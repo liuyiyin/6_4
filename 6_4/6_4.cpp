d6945183199719da466275014240bbe5cd110308
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-04-03 14:37:58 +0800

    6_4

--- 6_4/6_4.cpp
+++ 6_4/6_4.cpp
@@ -2,10 +2,32 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv.hpp>
+using namespace cv;
 
 int main()
 {
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("D:\\lena.jpg", 1);
+	if (srcMat.empty()) return -1;
+
+    cv::Point2f pts1[] = {
+		cv::Point2f(150, 150),
+		cv::Point2f(150, 300),
+		cv::Point2f(350, 300),
+		cv::Point2f(350,150) };
+    cv::Point2f pts2[] = {
+		cv::Point2f(200, 150),
+		cv::Point2f(200, 300),
+		cv::Point2f(340, 270),
+		cv::Point2f(340,180) };
+
+	cv::Mat perspective_matrix = cv::getPerspectiveTransform(pts1, pts2);
+	cv::warpPerspective(srcMat, dstMat, perspective_matrix, srcMat.size());
+	
+	cv::imshow("src", srcMat);
+	cv::imshow("dat", dstMat);
+	cv::waitKey(0);
     return 0;
 }
 
